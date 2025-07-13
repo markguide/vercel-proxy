f0e46af129f218ca4c40575566659f3089f26e04
markguide <48164607+markguide@users.noreply.github.com>
2025-07-13 19:27:17 +0200

    Update generate1.js

--- api/generate1.js
+++ api/generate1.js
@@ -14,8 +14,7 @@ export default async function handler(req, res) {
         model,
         messages,
         temperature,
-        max_tokens: 1500
-  //      max_tokens: max_tokens || 1500
+        max_tokens: max_tokens || 1500
       },
       {
         headers: {
