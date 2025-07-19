1c4be7ca487dfaded4b62a183a3a6aa11827b29f
markguide <48164607+markguide@users.noreply.github.com>
2025-07-19 18:30:14 +0200

    Update generate2.js

--- api/generate2.js
+++ api/generate2.js
@@ -1,10 +1,12 @@
 // /api/generate.js
 import { OpenAI } from 'openai';
 
-const openai = new OpenAI({
-//  apiKey: process.env.OPENAI_API_KEY, // ustaw w środowisku Vercel
-  apiKey:${process.env.OPENAI_API_KEY},
-});
+const openai = new OpenAI(
+  {
+  apiKey: process.env.OPENAI_API_KEY, // ustaw w środowisku Vercel
+//  apiKey: ${process.env.OPENAI_API_KEY},
+}
+);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
